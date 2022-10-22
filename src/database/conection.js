a8af80d4c3f4c4169e3686e2ff37d07ab8c34288
Nahuelnm <nahuel.n2o@gmail.com>
2022-10-22 12:27:40 -0300

    DBsettings

--- src/database/conection.js
+++ src/database/conection.js
@@ -5,6 +5,18 @@ const dbsettings ={
     password:'shaiya123',
     server:'127.0.0.1',
     database:'PS_UserData',
+    optios:{
+        encrypt:false,
+        trustServerCertificate: true,
+    }
 }
 
-sql.connect();
+
+
+async function getConnection(){
+    const pool = await sql.connect();
+    const result = await  pool.request().query('SELECT 1');
+    console.log(result)
+}
+
+getConnection();
